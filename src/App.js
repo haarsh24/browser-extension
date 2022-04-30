611f3cfc5693f582fdee544f1cb0bff45cef7e68
Kumar Harsh <kumarharsh241998@gmail.com>
2022-04-30 17:24:39 +0530

    Removed unused code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import { useEffect,useState } from 'react';
+
 import './App.css';
 import { Homepage } from './pages/Homepage';
-import axios from "axios";
+
 
 function App() {
-  const [background, setBackground] = useState('');
-  useEffect(() => {
-    const imageSource = axios.get("https://picsum.photos/200/300");
-    console.log(imageSource);
-    setBackground(imageSource);
-  },[])
+ 
   return (
     <div className="App  ">
    
